de0f48588b45ebfbe413c6b994567449cdb8c6cd
Christian <christianjao88@gmail.com>
2025-01-17 09:45:56 +0800

    Sign Up

--- src/routes/SignUp.jsx
+++ src/routes/SignUp.jsx
@@ -1,6 +1,42 @@
+import { useState } from "react";
+import CoolCheckbox from "../components/CoolCheckbox";
+import routeStyles from "../styles/route.module.css";
+import { Button0 } from "../components/Button";
+import { Input0 } from "../components/Input";
+
 const SignUp = () => {
+  const [showPassword, setShowPassword] = useState(false);
+  
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+  
   return (
-    <>Sign Up</>
+    <section className={routeStyles.authSection}>
+      <form onSubmit={handleSubmit}>
+        <div className={routeStyles.inputLabelPair}>
+          <label htmlFor="last">Email</label>
+          <Input0 type="email" placeholder="myemail@some-domain.com" id="email" name="email" />
+        </div>
+        
+        <div className={routeStyles.inputLabelPair}>
+          <label htmlFor="password">Password</label>
+          <Input0
+            type={!showPassword ? "password" : "text"}
+            placeholder="Your password"
+            id="password"
+            name="password"
+          />
+        </div>
+        
+        <div className={routeStyles.showPassword}>
+          <CoolCheckbox onChange={() => setShowPassword(!showPassword)} />
+          <label htmlFor="show-password">Show Password</label>
+        </div>
+
+        <Button0>Submit</Button0>
+      </form>
+    </section>
   );
 };
 
