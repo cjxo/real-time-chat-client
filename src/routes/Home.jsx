f720a1c4ebb582c393f7a73850fb46e3c2da60c0
Christian <christianjao88@gmail.com>
2025-01-20 11:14:30 +0800

    Basic messages UI

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,6 +1,117 @@
+import { Link } from "react-router-dom";
+import styles from "../styles/route.module.css";
+import { Input0 } from "../components/Input";
+
 const Home = () => {
   return (
-    <>Honey, I am home</>
+    <section className={styles.home}>
+      <h2 className={styles.title}>Messages</h2>
+      <div className={styles.searchBar}>
+        <img
+          src="./icons/search-alt-2-svgrepo-com.svg"
+          alt="search"
+        />
+        <Input0 type="search" placeholder="Search" />
+      </div>
+      
+      <ul>
+        <li>
+          <Link to="message/1">
+            <div className={styles.left}>
+              <img
+                src="./icons/profile-circle-svgrepo-com.svg"
+                alt="profile"
+              />
+            </div>
+            
+            <div className={styles.right}>
+              <p className={styles.name}>Name</p>
+              <p className={styles.recentMessage}>Recent Message</p>
+            </div>
+          </Link>
+        </li>
+        
+        <li>
+          <Link to="message/1">
+            <div className={styles.left}>
+              <img
+                src="./icons/profile-circle-svgrepo-com.svg"
+                alt="profile"
+              />
+            </div>
+            
+            <div className={styles.right}>
+              <p className={styles.name}>Name</p>
+              <p className={styles.recentMessage}>Recent Message</p>
+            </div>
+          </Link>
+        </li>
+        
+        <li>
+          <Link to="message/1">
+            <div className={styles.left}>
+              <img
+                src="./icons/profile-circle-svgrepo-com.svg"
+                alt="profile"
+              />
+            </div>
+            
+            <div className={styles.right}>
+              <p className={styles.name}>Name</p>
+              <p className={styles.recentMessage}>Recent Message</p>
+            </div>
+          </Link>
+        </li>
+        
+        <li>
+          <Link to="message/1">
+            <div className={styles.left}>
+              <img
+                src="./icons/profile-circle-svgrepo-com.svg"
+                alt="profile"
+              />
+            </div>
+            
+            <div className={styles.right}>
+              <p className={styles.name}>Name</p>
+              <p className={styles.recentMessage}>Recent Message</p>
+            </div>
+          </Link>
+        </li>
+        
+        <li>
+          <Link to="message/1">
+            <div className={styles.left}>
+              <img
+                src="./icons/profile-circle-svgrepo-com.svg"
+                alt="profile"
+              />
+            </div>
+            
+            <div className={styles.right}>
+              <p className={styles.name}>Name</p>
+              <p className={styles.recentMessage}>Recent Message</p>
+            </div>
+          </Link>
+        </li>
+        
+        <li>
+          <Link to="message/1">
+            <div className={styles.left}>
+              <img
+                src="./icons/profile-circle-svgrepo-com.svg"
+                alt="profile"
+              />
+            </div>
+            
+            <div className={styles.right}>
+              <p className={styles.name}>Name</p>
+              <p className={styles.recentMessage}>Recent Message</p>
+            </div>
+          </Link>
+        </li>
+      </ul>
+    </section>
   );
 };
 
