798d53c65cb20e351dd6fe4b9af1659022456949
Christian <64344406+cjxo@users.noreply.github.com>
2025-03-19 14:42:31 +0800

    Changed font location to assets

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "./index.css";
+import "./styles/index.css";
 
 // root of all
 import App from "./App.jsx";
